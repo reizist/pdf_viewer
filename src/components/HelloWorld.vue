b5cc4924d8f4b268a66406f0a47f0902c7ef2731
reizist <reizist@gmail.com>
2018-05-22 05:07:35 +0900

    Implement basic functions

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <input type="checkbox" v-model="show">
     <select v-model="src" style="width: 30em">
       <option v-for="item in pdfList" :value="item" v-text="item"></option>
     </select>
@@ -8,15 +7,24 @@
     <div class='container'>
       <div class='row main'>
         <div class='col-2'>
-          <button type='button' class='btn btn-light btn-control' v-on:click='prevPage()'>prev button</button>
+          <button type='button' class='btn btn-light btn-control' v-on:click='prevPage'>prev page</button>
         </div>
 
         <div class='col-8'>
-          <div v-if="loadedRatio > 0 && loadedRatio < 1"
-            style="background-color: green; color: white; text-align: center" :style="{ width: loadedRatio * 100 + '%' }">
-            {{ Math.floor(loadedRatio * 100) }}%
+          <div class="progress" style='height: 2px;'>
+            <div class='progress-bar bg-success' role='progressbar' v-if="loadedRatio > 0 && loadedRatio < 1"
+              v-bind:aria-valuenow="loadedRatio * 100" aria-valuemin="0" aria-valuemax="100" :style="{ width: loadedRatio * 100 + '%' }">
+            </div>
           </div>
-          <pdf v-if="show" ref="pdf" style="border: 1px solid red width: 30%;" :src="src" :page="page" @progress="loadedRatio = $event" @error="error" @num-pages="numPages = $event" @link-clicked="page = $event"></pdf>
+          <pdf ref="pdf" style="border: 1px solid red width: 30%;"
+            :src="src"
+            :page="page"
+            @progress="loadedRatio = $event"
+            @error="error"
+            @num-pages="numPages = $event"
+            @link-clicked="page = $event"
+            @loaded="resetPage()">
+          </pdf>
         </div>
 
         <div class='col-2'>
@@ -34,6 +42,7 @@ export default {
   components: {
     pdf,
   },
+
   data() {
     return {
       show: true,
@@ -48,19 +57,42 @@ export default {
       numPages: 0,
     };
   },
+
   methods: {
     error(err) {
       // eslint-disable-next-line
       console.log(err);
     },
 
+    resetPage() {
+      this.page = 1;
+    },
+
     prevPage() {
-      this.page = this.page - 1;
+      if (this.page > 1) {
+        this.page = this.page - 1;
+      }
     },
 
     nextPage() {
-      this.page = this.page + 1;
+      if (this.page < this.numPages) {
+        this.page = this.page + 1;
+      }
     },
+  },
+
+  created: function () {
+    var vm = this;
+    window.addEventListener('keydown', function(event) {
+      switch (event.key) {
+        case "ArrowLeft":
+          vm.prevPage();
+          break;
+        case "ArrowRight":
+          vm.nextPage();
+          break;
+      }
+    });
   },
 };
 </script>
@@ -70,5 +102,9 @@ export default {
   height: 600px;
 }
 .btn-control {
+}
+
+.progress {
+  margin: 3px 0;
 }
 </style>
